859e054ed0cce2646f8444389e2831910fb76bc2
Mike Manchee <mikemanchee@gmail.com>
2020-08-18 15:38:50 -0700

    add palindrome excersize to the project
    co-authored-by: David McCaslin <davidmccaslin94@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,3 @@
-$(document).ready(function() {
-  $("#userInput").submit(function(event) {
-    const input1 = parseInt($("input#input1").val());
-
-    if (isNaN(input1)) {
-      alert("Please enter a number and try again.");
-    } else {
-      const result1 = factorial(input1);
-
-      $("#result").text(result1);
-    }    
-    
-    event.preventDefault();
-  });
-});
-
-
 function factorial(number) {
   let output = number;
   // console.log(output);
@@ -25,3 +8,41 @@ function factorial(number) {
   }
   return output;
 }
+
+function isPalindrome(inputA) {
+  inputA = inputA.toUpperCase();
+  let inputB = inputA.split("").reverse().join("");
+  let message;
+  if (inputA === inputB) {
+    message = "Yes, it is a palendrome.";
+  } else {
+    message = "No, it is not a palendrome.";
+  }
+  return message;
+}
+
+$(document).ready(function() {
+  $("#userInput1").submit(function(event) {
+    const input1 = parseInt($("input#input1").val());
+
+    if (isNaN(input1)) {
+      alert("Please enter a number and try again.");
+    } else {
+      const result1 = factorial(input1);
+
+      $("#result1").text(result1);
+    }    
+    
+    event.preventDefault();
+  });
+  $("#userInput2").submit(function(event) {
+    const input2 = $("input#input2").val();
+    //alert(input2);
+    const result2 = isPalindrome(input2);
+
+    $("#result2").text(result2);
+    event.preventDefault();
+  });
+});
+
+
