d896ee649ae27cc6c2b26218b144a99607c17f92
Mike Manchee <mikemanchee@gmail.com>
2020-08-18 16:37:07 -0700

    add prime shifting to the project
    co-authored-by: David McCaslin <davidmccaslin94@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
 function factorial(number) {
   let output = number;
-  // console.log(output);
   for (let i = number; i > 1; i--) {
     number--;
     output = output * number;
-    // console.log(output);
   }
   return output;
 }
@@ -20,6 +18,48 @@ function isPalindrome(inputA) {
   }
   return message;
 }
+
+function primeShift(input) {
+	const primeArrayStatic;
+	for (let i = 2; i < input; i++) {
+		primeArrayStatic.push(i);
+	}
+
+	let primeArray = [];
+	let prime = 2;
+	primeArrayStatic.forEach(function(num) {
+		if (num % prime !== 0 || num === prime) {
+			primeArray.push(num);
+		}
+		prime++;
+	})
+	return primeArray;
+}
+
+function primeShift(input) {
+  let primeArray = [];
+  let prime = 1;
+
+  for (let i = 2; i < input; i++) {
+    prime++;
+    for (let e = 2; e < input; e++) {
+      if (i % prime === 0 && i !== prime) {
+        primeArray.push(i);
+      }
+    }    
+  }
+}
+
+recLoop(array, prime, num) {
+  
+  for (let i = num; i > prime; i--) {
+
+    if ()
+  }
+  prime++;
+  recLoop(array, prime, num);
+}
+
 
 $(document).ready(function() {
   $("#userInput1").submit(function(event) {
@@ -41,6 +81,13 @@ $(document).ready(function() {
     const result2 = isPalindrome(input2);
 
     $("#result2").text(result2);
+    event.preventDefault();
+  });
+  $("#userInput3").submit(function(event) {
+    const input3 = $("input#input3").val();
+    const result3 = primeShift(input3);
+
+    $("#result3").text(result3);
     event.preventDefault();
   });
 });
